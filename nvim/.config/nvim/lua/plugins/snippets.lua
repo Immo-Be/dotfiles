7697034aac161e20485b0e76bebbbf6e0b8af850
immobe <i.beeck@gmail.com>
2026-01-04 18:52:57 +0100

    fix: autosuggestion in bash files

--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -15,7 +15,7 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
-    -- This is related to abecodes/tabout.nvim, see https://github.com/abecodes/tabout.nvim
+		-- This is related to abecodes/tabout.nvim, see https://github.com/abecodes/tabout.nvim
 		keys = function()
 			-- Disable default tab keybinding in LuaSnip
 			return {}
@@ -28,7 +28,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-path",   -- Make sure path source is installed
+			"hrsh7th/cmp-path", -- Make sure path source is installed
 			"hrsh7th/cmp-buffer", -- Make sure buffer source is installed
 		},
 		config = function()
@@ -62,30 +62,20 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<leader>t"] = cmp.mapping.complete(), -- Explicitly trigger completion with <leader>t
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif require("luasnip").expand_or_jumpable() then
-							require("luasnip").expand_or_jump()
-						else
-							fallback() -- Fallback to actual tab character if nothing else
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif require("luasnip").jumpable(-1) then
-							require("luasnip").jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "path" },
+					-- { name = "path" },
+					-- there has been a problem where i use this command mainly to open bash command in nvim and the bashls (lsp) would get the temporariy path instead of the "curren path". this is a workaround, hopefully won't break anyting
+					{
+						name = "path",
+						option = {
+							get_cwd = function()
+								return vim.fn.getcwd()
+							end,
+						},
+					},
 				}, {
 					{ name = "buffer" },
 				}),
