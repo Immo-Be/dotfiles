c07f7267ebddf7b9bed5614a59686c02c6aa06e3
immobe <i.beeck@gmail.com>
2026-02-10 11:21:56 +0100

    Revert "revert: restore original tmux-navigator config"
    This reverts commit a73a4a19e550616c9063b6255d8082cea5acb272.

--- nvim/.config/nvim/lua/plugins/nvim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,13 +1,58 @@
 return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false, -- Ensure it's always loaded
+	init = function()
+		-- Disable default mappings from the plugin
+		vim.g.tmux_navigator_no_mappings = 1
+		vim.g.tmux_navigator_disable_when_zoomed = 1
+	end,
 	keys = {
-		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Move left" },
-		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Move down" },
-		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Move up" },
-		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Move right" },
+		{
+			"<C-h>",
+			function()
+				-- Check if we're in a floating/popup window
+				if vim.fn.win_gettype() == "popup" or vim.api.nvim_win_get_config(0).relative ~= "" then
+					vim.cmd("wincmd h")
+				else
+					vim.cmd("TmuxNavigateLeft")
+				end
+			end,
+			desc = "Move left",
+		},
+		{
+			"<C-j>",
+			function()
+				if vim.fn.win_gettype() == "popup" or vim.api.nvim_win_get_config(0).relative ~= "" then
+					vim.cmd("wincmd j")
+				else
+					vim.cmd("TmuxNavigateDown")
+				end
+			end,
+			desc = "Move down",
+		},
+		{
+			"<C-k>",
+			function()
+				if vim.fn.win_gettype() == "popup" or vim.api.nvim_win_get_config(0).relative ~= "" then
+					vim.cmd("wincmd k")
+				else
+					vim.cmd("TmuxNavigateUp")
+				end
+			end,
+			desc = "Move up",
+		},
+		{
+			"<C-l>",
+			function()
+				if vim.fn.win_gettype() == "popup" or vim.api.nvim_win_get_config(0).relative ~= "" then
+					vim.cmd("wincmd l")
+				else
+					vim.cmd("TmuxNavigateRight")
+				end
+			end,
+			desc = "Move right",
+		},
 		{ "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Move to last" },
 	},
 }
-
 
