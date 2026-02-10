45bede41e1a2d89e763abacd02f7e0597a57a9d2
immobe <i.beeck@gmail.com>
2026-02-10 11:14:01 +0100

    fix(tmux-navigator): disable in OpenCode floating/popup windows
    - Add config callback to detect OpenCode and popup windows
    - Override Ctrl-hjkl to use normal window navigation in those windows
    - Allows navigation between OpenCode and code windows without tmux interference

--- nvim/.config/nvim/lua/plugins/nvim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,12 +1,33 @@
 return {
-    "christoomey/vim-tmux-navigator",
-    lazy = false, -- Ensure it's always loaded
-  keys = {
-      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>",  desc = "Move left" },
-      { "<C-j>", "<cmd>TmuxNavigateDown<cr>",  desc = "Move down" },
-      { "<C-k>", "<cmd>TmuxNavigateUp<cr>",    desc = "Move up" },
-      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Move right" },
-      { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Move to last" },
-    }
+	"christoomey/vim-tmux-navigator",
+	lazy = false, -- Ensure it's always loaded
+	keys = {
+		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Move left" },
+		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Move down" },
+		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Move up" },
+		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Move right" },
+		{ "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Move to last" },
+	},
+	config = function()
+		-- Disable tmux navigator in OpenCode floating windows
+		vim.g.tmux_navigator_no_mappings = 0
+		vim.g.tmux_navigator_disable_when_zoomed = 1
+
+		-- Add autocmd to disable tmux navigation in OpenCode windows
+		vim.api.nvim_create_autocmd({ "FileType", "WinEnter" }, {
+			pattern = "*",
+			callback = function()
+				local ft = vim.bo.filetype
+				-- Check if this is an OpenCode window (adjust pattern as needed)
+				if ft == "opencode" or vim.fn.win_gettype() == "popup" then
+					-- In OpenCode windows, use normal Ctrl-hjkl for window navigation
+					vim.keymap.set("n", "<C-h>", "<C-w>h", { buffer = true, silent = true })
+					vim.keymap.set("n", "<C-j>", "<C-w>j", { buffer = true, silent = true })
+					vim.keymap.set("n", "<C-k>", "<C-w>k", { buffer = true, silent = true })
+					vim.keymap.set("n", "<C-l>", "<C-w>l", { buffer = true, silent = true })
+				end
+			end,
+		})
+	end,
 }
 
