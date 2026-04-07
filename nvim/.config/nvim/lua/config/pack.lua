38518cad79fcfbb8e868e8f3c91be46bc512c714
immobe <i.beeck@gmail.com>
2026-04-07 15:27:34 +0200

    fix(treesitter): migrate to new API and pin core

--- nvim/.config/nvim/lua/config/pack.lua
+++ nvim/.config/nvim/lua/config/pack.lua
@@ -1,66 +1,5 @@
-local pinned_versions = {
-	["LuaSnip"] = "a62e1083a3cfe8b6b206e7d3d33a51091df25357",
-	["avante.nvim"] = "8c84af0f09e5d9058362ac58401a8e7b047d826e",
-	["catppuccin"] = "426dbebe06b5c69fd846ceb17b42e12f890aedf1",
-	["cmp-buffer"] = "b74fab3656eea9de20a9b8116afa3cfc4ec09657",
-	["cmp-nvim-lsp"] = "cbc7b02bb99fae35cb42f514762b89b5126651ef",
-	["cmp-path"] = "c642487086dbd9a93160e1679a1327be111cbc25",
-	["cmp_luasnip"] = "98d9cb5c2c38532bd9bdb481067b20fea8f32e90",
-	["conform.nvim"] = "086a40dc7ed8242c03be9f47fbcee68699cc2395",
-	["copilot.lua"] = "07aa57148ac28986bab9f55e87ee3d929e2726b1",
-	["diffview.nvim"] = "4516612fe98ff56ae0415a259ff6361a89419b0a",
-	["dressing.nvim"] = "2d7c2db2507fa3c4956142ee607431ddb2828639",
-	["emmet-vim"] = "92ef2f74f4093edc99db5e9e4cf7e40116a85bd6",
-	["friendly-snippets"] = "6cd7280adead7f586db6fccbd15d2cac7e2188b9",
-	["fzf"] = "18315000185a6e6461b9b4aa4a4cb6cd164e0e35",
-	["fzf-lua"] = "9f0432fdd7825ab163520045831a40b6df82ea28",
-	["git-conflict.nvim"] = "4bbfdd92d547d2862a75b4e80afaf30e73f7bbb4",
-	["gitsigns.nvim"] = "0d797daee85366bc242580e352a4f62d67557b84",
-	["img-clip.nvim"] = "b6ddfb97b5600d99afe3452d707444afda658aca",
-	["indent-blankline.nvim"] = "d28a3f70721c79e3c5f6693057ae929f3d9c0a03",
-	["lualine.nvim"] = "8811f3f3f4dc09d740c67e9ce399e7a541e2e5b2",
-	["mason-lspconfig.nvim"] = "25f609e7fca78af7cede4f9fa3af8a94b1c4950b",
-	["mason-tool-installer.nvim"] = "443f1ef8b5e6bf47045cb2217b6f748a223cf7dc",
-	["mason.nvim"] = "b03fb0f20bc1d43daf558cda981a2be22e73ac42",
-	["mini.comment"] = "8e5ff3ed3cc0e8f216617aae01020c00c20f7a87",
-	["mini.files"] = "ced297546b8fdb8e215d416d4753a735514a2fe0",
-	["mini.icons"] = "7fdae2443a0e2910015ca39ad74b50524ee682d3",
-	["mini.pick"] = "fd7e7efadddcec3f3d7f3b363a99aa44e7286c65",
-	["neo-tree.nvim"] = "84c75e7a7e443586f60508d12fc50f90d9aee14e",
-	["neogit"] = "5a7fca171e3ad07380745d573d791e95268b8f3f",
-	["none-ls-extras.nvim"] = "14fa31ce8c0268a3b2c9cc14979ecf771982d433",
-	["none-ls.nvim"] = "7f9301e416533b5d74e2fb3b1ce5059eeaed748b",
-	["nui.nvim"] = "de740991c12411b663994b2860f1a4fd0937c130",
-	["nvim-autopairs"] = "59bce2eef357189c3305e25bc6dd2d138c1683f5",
-	["nvim-bqf"] = "c282a62bec6c0621a1ef5132aa3f4c9fc4dcc2c7",
-	["nvim-cmp"] = "a1d504892f2bc56c2e79b65c6faded2fd21f3eca",
-	["nvim-colorizer.lua"] = "a065833f35a3a7cc3ef137ac88b5381da2ba302e",
-	["nvim-lint"] = "4b03656c09c1561f89b6aa0665c15d292ba9499d",
-	["nvim-lspconfig"] = "bedca8b426b2fee0ccac596d167d71bbe971253f",
-	["nvim-surround"] = "1098d7b3c34adcfa7feb3289ee434529abd4afd1",
-	["nvim-treesitter"] = "cf12346a3414fa1b06af75c79faebe7f76df080a",
-	["nvim-treesitter-context"] = "b0c45cefe2c8f7b55fc46f34e563bc428ef99636",
-	["nvim-treesitter-textobjects"] = "5ca4aaa6efdcc59be46b95a3e876300cfead05ef",
-	["nvim-ts-autotag"] = "8e1c0a389f20bf7f5b0dd0e00306c1247bda2595",
-	["nvim-web-devicons"] = "95b7a002d5dba1a42eb58f5fac5c565a485eefd0",
-	["oil.nvim"] = "0fcc83805ad11cf714a949c98c605ed717e0b83e",
-	["opencode.nvim"] = "fa0a495fa2c229115404cb3c1970578f9c6d2f76",
-	["plenary.nvim"] = "b9fd5226c2f76c951fc8ed5923d85e4de065e509",
-	["rainbow-delimiters.nvim"] = "aab6caaffd79b8def22ec4320a5344f7c42f58d2",
-	["render-markdown.nvim"] = "4ae2f2e8e8c66d070f33cfb57cb6f867e3baf5d9",
-	["snacks.nvim"] = "ad9ede6a9cddf16cedbd31b8932d6dcdee9b716e",
-	["sqlite.lua"] = "50092d60feb242602d7578398c6eb53b4a8ffe7b",
-	["telescope-ui-select.nvim"] = "6e51d7da30bd139a6950adf2a47fda6df9fa06d2",
-	["telescope.nvim"] = "a0bbec21143c7bc5f8bb02e0005fa0b982edc026",
-	["trouble.nvim"] = "bd67efe408d4816e25e8491cc5ad4088e708a69a",
-	["vim-illuminate"] = "0d1e93684da00ab7c057410fecfc24f434698898",
-	["vim-tmux-navigator"] = "e41c431a0c7b7388ae7ba341f01a0d217eb3a432",
-	["which-key.nvim"] = "3aab2147e74890957785941f0c1ad87d0a44c15a",
-}
-
 local function gh(repo, opts)
 	opts = opts or {}
-	local name = opts.name or repo:match("/([^/]+)$")
 	local spec = {
 		src = "https://github.com/" .. repo,
 	}
@@ -69,8 +8,8 @@ local function gh(repo, opts)
 		spec.name = opts.name
 	end
 
-	if pinned_versions[name] then
-		spec.version = pinned_versions[name]
+	if opts.version then
+		spec.version = opts.version
 	end
 
 	return spec
@@ -159,7 +98,7 @@ local pack_specs = {
 	gh("hrsh7th/nvim-cmp"),
 	gh("windwp/nvim-autopairs"),
 	gh("christoomey/vim-tmux-navigator"),
-	gh("nvim-treesitter/nvim-treesitter"),
+	gh("nvim-treesitter/nvim-treesitter", { version = "4916d6592ede8c07973490d9322f187e07dfefac" }),
 	gh("windwp/nvim-ts-autotag"),
 	gh("HiPhish/rainbow-delimiters.nvim"),
 	gh("nvim-treesitter/nvim-treesitter-textobjects"),
@@ -230,6 +169,14 @@ local setup_modules = {
 
 require("plugins.nvim-tmux-navigator").init()
 setup_build_hooks()
+
+vim.api.nvim_create_user_command("PackUpdateAll", function()
+	vim.pack.update()
+end, { desc = "Update all vim.pack plugins" })
+
+vim.api.nvim_create_user_command("PackUpdateLockfile", function()
+	vim.pack.update(nil, { offline = true, target = "lockfile" })
+end, { desc = "Sync plugins to nvim-pack-lock.json" })
 
 vim.pack.add(pack_specs, { confirm = false, load = true })
 
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,111 +1,203 @@
 local M = {}
 
-function M.setup()
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = {
-			"markdown",
-			"markdown_inline",
-			"toml",
-			"yaml",
-			"go",
-			"gotmpl",
-			"javascript",
-			"typescript",
-			"tsx", -- TypeScript + React TSX
-			"svelte",
-			"html",
-			"css",
-			"json", -- Web dev essentials
-			"lua",
-			"vim", -- Neovim scripting
-			"astro",
-		},
-		sync_install = false,
-		highlight = { enable = true, additional_vim_regex_highlighting = false },
-		indent = { enable = true },
-		autotag = { enable = true }, -- Auto-close & rename JSX/Svelte tags
-		-- Note: rainbow-delimiters is configured separately below (not in treesitter config)
+local desired_parsers = {
+	"markdown",
+	"markdown_inline",
+	"toml",
+	"yaml",
+	"go",
+	"gotmpl",
+	"javascript",
+	"typescript",
+	"tsx",
+	"svelte",
+	"html",
+	"css",
+	"json",
+	"lua",
+	"vim",
+	"astro",
+}
 
-		-- ⬇️ Incremental selection = grow/shrink by syntax node
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<CR>", -- Start selection with Enter
-				node_incremental = "<CR>", -- Expand selection with Enter
-				node_decremental = "-", -- Shrink selection with -
-				scope_incremental = "grc",
-			},
-		},
+local desired_parser_set = {}
+for _, parser in ipairs(desired_parsers) do
+	desired_parser_set[parser] = true
+end
 
-		matchup = { enable = true }, -- optional, enhances tag motions
-		-- Treesitter Textobjects
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-					["al"] = "@loop.outer",
-					["il"] = "@loop.inner",
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true,
-				goto_next_start = {
-					["]m"] = { query = "@function.outer", desc = "Next function" },
-					-- these are not used becase we use [ / [c for navigating between changes
-					-- ["]c"] = { query = "@class.outer", desc = "Next class" },
-					["]a"] = { query = "@parameter.outer", desc = "Next parameter" },
-					["]r"] = { query = "@return.outer", desc = "Next return statement" },
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]C"] = "@class.outer",
-					["]A"] = "@parameter.outer",
-				},
-				goto_previous_start = {
-					["[m"] = { query = "@function.outer", desc = "Previous function" },
-					-- ["[c"] = { query = "@class.outer", desc = "Previous class" },
-					["[a"] = { query = "@parameter.outer", desc = "Previous parameter" },
-					["[r"] = { query = "@return.outer", desc = "Previous return statement" }, -- Jump to previous return statement
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[C"] = "@class.outer",
-					["[A"] = "@parameter.outer",
-				},
-			},
-			swap = {
-				enable = true,
-				swap_next = {
-					["<leader>sp"] = "@parameter.inner",
-				},
-				swap_previous = {
-					["<leader>sP"] = "@parameter.inner",
-				},
-			},
+local function ensure_parsers_installed()
+	if #vim.api.nvim_list_uis() == 0 then
+		return
+	end
+
+	local treesitter = require("nvim-treesitter")
+	local installed = {}
+	for _, parser in ipairs(treesitter.get_installed()) do
+		installed[parser] = true
+	end
+
+	local missing = {}
+	for _, parser in ipairs(desired_parsers) do
+		if not installed[parser] then
+			table.insert(missing, parser)
+		end
+	end
+
+	if #missing == 0 then
+		return
+	end
+
+	vim.schedule(function()
+		treesitter.install(missing, { summary = true })
+	end)
+end
+
+local function enable_treesitter(bufnr)
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
+	if not vim.api.nvim_buf_is_valid(bufnr) or not vim.bo[bufnr].buflisted then
+		return
+	end
+
+	local filetype = vim.bo[bufnr].filetype
+	if filetype == "" then
+		return
+	end
+
+	local lang = vim.treesitter.language.get_lang(filetype)
+	if not lang then
+		return
+	end
+
+	local ok = pcall(vim.treesitter.start, bufnr, lang)
+	if not ok then
+		return
+	end
+
+	if desired_parser_set[lang] then
+		vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		vim.bo[bufnr].syntax = "off"
+	end
+end
+
+local function setup_textobjects()
+	require("nvim-treesitter-textobjects").setup({
+		select = {
+			lookahead = true,
+		},
+		move = {
+			set_jumps = true,
 		},
 	})
 
-	vim.keymap.set("x", "+", "<CR>", { remap = true, desc = "Expand selection (alias for Enter)" })
+	local select = require("nvim-treesitter-textobjects.select")
+	local move = require("nvim-treesitter-textobjects.move")
+	local swap = require("nvim-treesitter-textobjects.swap")
+
+	local select_keymaps = {
+		["af"] = "@function.outer",
+		["if"] = "@function.inner",
+		["ac"] = "@class.outer",
+		["ic"] = "@class.inner",
+		["al"] = "@loop.outer",
+		["il"] = "@loop.inner",
+		["aa"] = "@parameter.outer",
+		["ia"] = "@parameter.inner",
+	}
+
+	for lhs, query in pairs(select_keymaps) do
+		vim.keymap.set({ "x", "o" }, lhs, function()
+			select.select_textobject(query, "textobjects")
+		end)
+	end
+
+	local next_start = {
+		["]m"] = { query = "@function.outer", desc = "Next function" },
+		["]a"] = { query = "@parameter.outer", desc = "Next parameter" },
+		["]r"] = { query = "@return.outer", desc = "Next return statement" },
+	}
+
+	for lhs, entry in pairs(next_start) do
+		vim.keymap.set({ "n", "x", "o" }, lhs, function()
+			move.goto_next_start(entry.query, "textobjects")
+		end, { desc = entry.desc })
+	end
+
+	local next_end = {
+		["]M"] = { query = "@function.outer", desc = "Next function end" },
+		["]C"] = { query = "@class.outer", desc = "Next class end" },
+		["]A"] = { query = "@parameter.outer", desc = "Next parameter end" },
+	}
+
+	for lhs, entry in pairs(next_end) do
+		vim.keymap.set({ "n", "x", "o" }, lhs, function()
+			move.goto_next_end(entry.query, "textobjects")
+		end, { desc = entry.desc })
+	end
+
+	local prev_start = {
+		["[m"] = { query = "@function.outer", desc = "Previous function" },
+		["[a"] = { query = "@parameter.outer", desc = "Previous parameter" },
+		["[r"] = { query = "@return.outer", desc = "Previous return statement" },
+	}
+
+	for lhs, entry in pairs(prev_start) do
+		vim.keymap.set({ "n", "x", "o" }, lhs, function()
+			move.goto_previous_start(entry.query, "textobjects")
+		end, { desc = entry.desc })
+	end
+
+	local prev_end = {
+		["[M"] = { query = "@function.outer", desc = "Previous function end" },
+		["[C"] = { query = "@class.outer", desc = "Previous class end" },
+		["[A"] = { query = "@parameter.outer", desc = "Previous parameter end" },
+	}
+
+	for lhs, entry in pairs(prev_end) do
+		vim.keymap.set({ "n", "x", "o" }, lhs, function()
+			move.goto_previous_end(entry.query, "textobjects")
+		end, { desc = entry.desc })
+	end
+
+	vim.keymap.set("n", "<leader>sp", function()
+		swap.swap_next("@parameter.inner")
+	end, { desc = "Swap with next parameter" })
+
+	vim.keymap.set("n", "<leader>sP", function()
+		swap.swap_previous("@parameter.inner")
+	end, { desc = "Swap with previous parameter" })
+end
+
+function M.setup()
+	ensure_parsers_installed()
+
+	local treesitter_group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = treesitter_group,
+		pattern = "*",
+		callback = function(args)
+			enable_treesitter(args.buf)
+		end,
+	})
+
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		enable_treesitter(bufnr)
+	end
+
+	require("nvim-ts-autotag").setup({
+		opts = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = false,
+		},
+	})
+
+	setup_textobjects()
 
 	require("treesitter-context").setup({
 		enable = true,
-		throttle = true,
 		max_lines = 2,
-		patterns = {
-			default = {
-				"class",
-				"function",
-				"method",
-			},
-		},
+		multiline_threshold = 20,
+		trim_scope = "outer",
+		mode = "cursor",
 	})
 
 	vim.keymap.set("n", "[f", function()
