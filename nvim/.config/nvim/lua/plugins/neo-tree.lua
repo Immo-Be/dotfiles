7e92a77081719d9a2483ded048167287f2310001
immobe <i.beeck@gmail.com>
2026-05-16 09:30:15 +0200

    feat(nvim): add neo-tree component scaffold

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,62 @@
 local M = {}
 
+local function pascal_case(name)
+	return (name:gsub("[%-%_ ](%w)", function(c)
+		return c:upper()
+	end):gsub("^%w", string.upper))
+end
+
+local function get_default_parent_dir()
+	local bufname = vim.api.nvim_buf_get_name(0)
+	if bufname ~= "" then
+		return vim.fs.dirname(vim.fn.fnamemodify(bufname, ":p"))
+	end
+
+	return vim.fn.getcwd()
+end
+
+local function create_component(parent_dir, component_name)
+	local component_dir = vim.fs.joinpath(parent_dir, component_name)
+	if vim.uv.fs_stat(component_dir) then
+		vim.notify("Component folder already exists: " .. component_dir, vim.log.levels.ERROR)
+		return
+	end
+
+	vim.fn.mkdir(component_dir, "p")
+
+	local tsx_path = vim.fs.joinpath(component_dir, component_name .. ".tsx")
+	local css_path = vim.fs.joinpath(component_dir, component_name .. ".module.css")
+	local component_fn = pascal_case(component_name)
+
+	vim.fn.writefile({
+		'import styles from "./' .. component_name .. '.module.css"',
+		"",
+		"type Props = {};",
+		"",
+		"export default function " .. component_fn .. "({}: Props) {",
+		"  return (",
+		'    <div className={styles.default}>',
+		"    </div>",
+		"  )",
+		"}",
+	}, tsx_path)
+
+	vim.fn.writefile({}, css_path)
+
+	vim.notify("Created component: " .. component_dir)
+	vim.cmd.edit(vim.fn.fnameescape(tsx_path))
+end
+
+local function prompt_component_name(parent_dir)
+	vim.ui.input({ prompt = "Component name: " }, function(input)
+		if not input or input == "" then
+			return
+		end
+
+		create_component(parent_dir, input)
+	end)
+end
+
 function M.setup()
 	require("neo-tree").setup({
 		commands = {
@@ -22,6 +79,21 @@ function M.setup()
 				vim.fn.setreg("*", path)
 				vim.notify("Copied path: " .. path)
 			end,
+			new_component = function(state)
+				local node = state.tree:get_node()
+				local parent_dir = get_default_parent_dir()
+
+				if node then
+					local node_path = node:get_id()
+					if vim.fn.isdirectory(node_path) == 1 then
+						parent_dir = node_path
+					else
+						parent_dir = vim.fs.dirname(node_path)
+					end
+				end
+
+				prompt_component_name(parent_dir)
+			end,
 		},
 		filesystem = {
 			follow_current_file = { enabled = true },
@@ -31,6 +103,7 @@ function M.setup()
 				mappings = {
 					["O"] = "open_in_finder",
 					["Y"] = "copy_path",
+					["C"] = "new_component",
 				},
 			},
 			filtered_items = {
@@ -49,6 +122,10 @@ function M.setup()
 	vim.api.nvim_create_user_command("E", function()
 		vim.cmd("Neotree current")
 	end, { desc = "Open Neo-tree in current buffer" })
+
+	vim.api.nvim_create_user_command("NewComponent", function()
+		prompt_component_name(get_default_parent_dir())
+	end, { desc = "Create a new React component folder" })
 end
 
 return M
