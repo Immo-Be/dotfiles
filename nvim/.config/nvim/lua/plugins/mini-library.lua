6269df70a0a57901eadf76a081e065c37323b807
immobe <i.beeck@gmail.com>
2026-05-22 22:46:07 +0200

    feat(nvim/mini-files): add split open support
    - Refactor file opening logic to support vertical splits
    - Add 's' keymap to open files in vertical split
    - Use close_on_file option for cleaner file opening behavior

--- nvim/.config/nvim/lua/plugins/mini-library.lua
+++ nvim/.config/nvim/lua/plugins/mini-library.lua
@@ -22,17 +22,36 @@ function M.setup()
 		pattern = "MiniFilesBufferCreate",
 		callback = function(args)
 			local buf_id = args.data.buf_id
-			vim.keymap.set("n", "l", function()
+			local function open_entry(open_in_split)
 				local fs_entry = mini_files.get_fs_entry()
-				if fs_entry and fs_entry.fs_type == "file" then
-					mini_files.go_in()
-					vim.schedule(function()
-						mini_files.close()
-					end)
+				if not fs_entry then
+					return
+				end
+
+				if fs_entry.fs_type == "file" then
+					if open_in_split then
+						local cur_target = mini_files.get_explorer_state().target_window
+						local new_target = vim.api.nvim_win_call(cur_target, function()
+							vim.cmd("belowright vertical split")
+							return vim.api.nvim_get_current_win()
+						end)
+
+						mini_files.set_target_window(new_target)
+						mini_files.go_in({ close_on_file = true })
+					else
+						mini_files.go_in({ close_on_file = true })
+					end
 				else
 					mini_files.go_in()
 				end
+			end
+
+			vim.keymap.set("n", "l", function()
+				open_entry()
 			end, { buffer = buf_id })
+			vim.keymap.set("n", "s", function()
+				open_entry(true)
+			end, { buffer = buf_id, desc = "Open in vertical split" })
 		end,
 	})
 end
