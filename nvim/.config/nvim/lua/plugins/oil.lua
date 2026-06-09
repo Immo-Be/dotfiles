246bda7aa2f9fb8105f0abd35294e892ab2921ce
immobe <i.beeck@gmail.com>
2026-06-09 12:40:13 +0200

    Add Oil path copy keymap

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,28 @@
 local M = {}
 
 function M.setup()
-	require("oil").setup()
+	local oil = require("oil")
+
+	oil.setup({
+		keymaps = {
+			Y = function()
+				local entry = oil.get_cursor_entry()
+				if not entry then
+					return
+				end
+
+				local path = entry.path
+				if not path then
+					local dir = oil.get_current_dir(0) or vim.fn.getcwd()
+					path = vim.fs.normalize(vim.fs.joinpath(dir, entry.name))
+				end
+
+				vim.fn.setreg("+", path)
+				vim.fn.setreg("*", path)
+				vim.notify("Copied path: " .. path)
+			end,
+		},
+	})
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 end
 
